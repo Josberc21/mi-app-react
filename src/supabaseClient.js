de8b22b71a0a18dcd19ac1218e247edd3060b0fc
Joseph Rios <jb.rios@pascualbravo.edu.co>
2025-10-08 12:05:39 -0500

    refactor: Mover credenciales a variables de entorno

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mgdjaxmivlgdcvgchdfe.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZGpheG1pdmxnZGN2Z2NoZGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyOTIzNzksImV4cCI6MjA3NDg2ODM3OX0.5VQ4m5DrmOnShltkwU1XH_TPh0_hBi7pbfLPlUc-U5U'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
